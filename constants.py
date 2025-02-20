2e3eda5e2a60ed53500147089022143a7d03c102
Frederic Ferriere <57955693+FredericFerriere@users.noreply.github.com>
2025-02-20 20:33:40 +0100

    initialisation

--- constants.py
+++ constants.py
@@ -3,6 +3,7 @@ CENTER_LONGITUDE = 6.11667
 ROOT_PATH = r'C:\Users\frede\Galaxy_dev\test_data\outputs'
 MODEL_MAP = {'van':['van', 'Van'],
             'bike':['bike', 'Bike'],
-             'bike_bus_zoned':['bike_bus_zoned', 'Bike & Bus Zoned'],
-            'bike_bus_line':['bike_bus_line', 'Bike & Bus Line']
+            'bike_bus_zoned':['bike_bus_zoned', 'Bike & Bus Zoned'],
+            'bike_bus_line':['bike_bus_line', 'Bike & Bus Line'],
+            'bike_bus_hub':['bike_bus_hub', 'Bike & Bus Hub']
              }
---
+++ views/st_bike_bus_hub_model.py
@@ -0,0 +1,5 @@
+from data import model as m
+import views.st_model_page as mp
+
+cur_model = m.Model.get_model('bike_bus_hub')
+mp.view_model(cur_model)
